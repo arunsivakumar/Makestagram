46e0000c45b9aa724709fca659589ff61ad1ff1f
Arun Sivakumar <lakshmi@Aruns-MacBook-Pro.local>
2017-07-09 12:28:09 -0600

    FB

--- Makestagram/AppDelegate.swift
+++ Makestagram/AppDelegate.swift
@@ -17,6 +17,27 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     var window: UIWindow?
     
+    var parseLoginHelper: ParseLoginHelper!
+    
+    override init() {
+        super.init()
+        
+        parseLoginHelper = ParseLoginHelper {[unowned self] user, error in
+            // Initialize the ParseLoginHelper with a callback
+            if let error = error {
+                // 1
+                ErrorHandling.defaultErrorHandler(error)
+            } else  if let _ = user {
+                // if login was successful, display the TabBarController
+                // 2
+                let storyboard = UIStoryboard(name: "Main", bundle: nil)
+                let tabBarController = storyboard.instantiateViewControllerWithIdentifier("TabBarController")
+                // 3
+                self.window?.rootViewController!.presentViewController(tabBarController, animated:true, completion:nil)
+            }
+        }
+    }
+    
     
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
@@ -37,25 +58,69 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                 acl.publicReadAccess = true
                 PFACL.setDefaultACL(acl, withAccessForCurrentUser: true)
         
-        do {
-            try PFUser.logInWithUsername("test", password: "test")
-        } catch {
-            print("Unable to log in")
+//        do {
+//            try PFUser.logInWithUsername("test", password: "test")
+//        } catch {
+//            print("Unable to log in")
+//        }
+//        
+//        if let currentUser = PFUser.currentUser() {
+//            print("\(currentUser.username!) logged in successfully")
+//            // 2
+//            //        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+//            //        let tabBarController = storyboard.instantiateViewControllerWithIdentifier("TabBarController")
+//            //        // 3
+//            //        self.window?.rootViewController!.presentViewController(tabBarController, animated:true, completion:nil)
+//        } else {
+//            print("No logged in user :(")
+//        }
+        
+        // Initialize Facebook
+        // 1
+        PFFacebookUtils.initializeFacebookWithApplicationLaunchOptions(launchOptions)
+        
+        // check if we have logged in user
+        // 2
+        let user = PFUser.currentUser()
+        
+        let startViewController: UIViewController
+        
+        if (user != nil) {
+            // 3
+            // if we have a user, set the TabBarController to be the initial view controller
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            startViewController = storyboard.instantiateViewControllerWithIdentifier("TabBarController") as! UITabBarController
+        } else {
+            // 4
+            // Otherwise set the LoginViewController to be the first
+            let loginViewController = PFLogInViewController()
+            loginViewController.fields = [.UsernameAndPassword, .LogInButton, .SignUpButton, .PasswordForgotten, .Facebook]
+            loginViewController.delegate = parseLoginHelper
+            loginViewController.signUpController?.delegate = parseLoginHelper
+            
+            startViewController = loginViewController
         }
         
-        if let currentUser = PFUser.currentUser() {
-            print("\(currentUser.username!) logged in successfully")
-            // 2
-            //        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-            //        let tabBarController = storyboard.instantiateViewControllerWithIdentifier("TabBarController")
-            //        // 3
-            //        self.window?.rootViewController!.presentViewController(tabBarController, animated:true, completion:nil)
-        } else {
-            print("No logged in user :(")
-        }
+        // 5
+        self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        self.window?.rootViewController = startViewController;
+        self.window?.makeKeyAndVisible()
         
         return true
     }
+    
+    //MARK: Facebook Integration
+    
+    func applicationDidBecomeActive(application: UIApplication) {
+        FBSDKAppEvents.activateApp()
+    }
+    
+    func application(application: UIApplication, openURL url: NSURL, sourceApplication: String?, annotation: AnyObject) -> Bool {
+        return FBSDKApplicationDelegate.sharedInstance().application(application, openURL: url, sourceApplication: sourceApplication, annotation: annotation)
+    }
+    
+    
+    
     
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
@@ -71,9 +136,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
     }
     
-    func applicationDidBecomeActive(application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
+//    func applicationDidBecomeActive(application: UIApplication) {
+//        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+//    }
     
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
